45d88efef7a5f4c1c1ae185fd31c08ff52a76b9e
AlfredChenZH <49675655+AlfredChenZH@users.noreply.github.com>
2019-12-02 20:09:04 +0800

    Delete Alpha103.m

--- 002 src/02 factor_conduct/3/Alpha103.m
+++
@@ -1,9 +0,0 @@
-% ((20 - LOWDAY(LOW, 20)) / 20) * 100
-
-function X = alpha103(stock)
-    X = getAlpha103(stock.low);
-end
-
-function exposure = getAlpha103(low)
-    exposure = (20 - lowday(low, 20))./ 20.* 100;
-end
