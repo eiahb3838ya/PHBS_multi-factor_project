beb6c60be7bbfd4877e203d00c8ab79e361c9ebe
ZhangjieLyu <43562276+ZhangjieLyu@users.noreply.github.com>
2019-12-03 15:09:47 +0800

    Update sumPast.m

--- 002 src/02 factor_conduct/utils/sumPast.m
+++ 002 src/02 factor_conduct/utils/sumPast.m
@@ -1,9 +1,8 @@
 function sumPast = sumPast(A, n)
 %sumPast: moving sum in a matrix way
     sumPast = zeros(size(A));
-    for i = 1:n-1
+    for i = 0:n-1
         toAdd = [zeros(i, size(A, 2));A(1:end-i,:)];
         sumPast = sumPast + toAdd;
     end
-    sumPast = sumPast + A;
 end
