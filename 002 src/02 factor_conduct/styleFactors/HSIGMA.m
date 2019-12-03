54dde7e95e03b745556ef08e72734d23f3eb0905
ytfang222 <54805661+ytfang222@users.noreply.github.com>
2019-12-03 17:44:27 +0800

    Update HSIGMA.m

--- 002 src/02 factor_conduct/styleFactors/HSIGMA.m
+++ 002 src/02 factor_conduct/styleFactors/HSIGMA.m
@@ -1,18 +1,31 @@
-function X = HSIGMA(stock)
-    [X,offsetSize] = getAlpha(stock.properties.totalreturns);
+function [X, offsetSize] = BETA(stock)
+% Computed as the slope coefficient in a time-series reg of excess stock return.
+% the benchmark is zz500.
+% r_t = alpha + beta * R_t +eplison
+% stock is a structure
+
+% clean data module here
+
+% get factor module here
+    [X, offsetSize] = getAlpha(stock.properties.totalReturn;
+                              stock.index.totalReturn[:,7]);
 end
 
-%beta zz500
-function [factor offsetSize] = getAlpha(stockrts,ZZ500rts)
+%-------------------------------------------------------------------------
+function [exposure, offsetSize] = getAlpha(stockrts,ZZ500rts)
     [m,n]= size(stockrts);
 	w = ExponentialWeight(252, 63);
-    wMatrix =  repmat(w,1,n); 
+    wMatrix = repmat(w,1,n); 
     
     for i = 252:m
         wStockRts = stockrts(i-251:i,:) .* wMatrix;
         wZZ500Rts = ZZ500rts(i-251:i,:) .* w;
-        beta(i,:)=(wStockRts' * wStockRts \ (wStockRts' * wZZ500Rts ))'
-        res(i,:) = wZZ500Rts(i,1) - wStockRts * beta';
+        toCell =mat2cell(wStockRts,252,[ones(1,n)]);
+        B =blkdiag(toCell{:});%diag each column of the wStockRts
+        Y = repmat(wZZ500Rts,n,1);
+        beta(i,:) = (B'*B\(B' * Y ))';
     end
+    
+    exposure = beta;
+    offsetSize = 521;
 end
-
