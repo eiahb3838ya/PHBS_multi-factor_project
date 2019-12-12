1b66ccfe7665fe08a2b32a87d57046cbaea6ae96
ytfang222 <yt_fang222@163.com>
2019-12-12 23:04:05 +0800

    Create singFactorTest

--- 002 src/05 singleFactorTest/rankICValue.m
+++ 002 src/05 singleFactorTest/rankICValue.m
@@ -1,17 +1,22 @@
-function IC = ICValue(alpha,day,rollingWindow,d,alphaPara)
-
+function IC = rankICValue(alpha,day,rollingWindow,d,alphaPara)
 try
         close = alphaPara.close;
+       
     catch
         error 'para error';
 end
     
 getAlpha = alpha;
+[~, Xidx] = sort(getAlpha,2);
+[~,Xidx2] = sort(Xidx,2);
+getAlha = Xidx2;
 
 [m, n] = size(alphaPara.close);
 delayclose = [zeros(1, n);alphaPara.close(1: m - 1,:)];
 rts = close./(delayclose + eps) - 1;
-%rts (find(isnan(rts)==1)) = 0; % rank 
+[~, Xidxx] = sort(rts,2);
+[~,Xidxx2] = sort(Xidxx,2);
+rts = Xidxx2;
 
 for i = day - rollingWindow : day - d
 corrMatrix = corrcoef(getAlpha(i-(day - rollingWindow) +1,:),rts(i-(day - rollingWindow) +1 +d,:));
