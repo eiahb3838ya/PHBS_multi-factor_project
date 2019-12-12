8e2f8fa5f755b07558baaabdd1aa01df8e4c0295
AlfredChenZH <alfredchen0228@163.com>
2019-12-12 19:44:35 +0800

    little adj & data demo

--- 002 src/02 factor_conduct/04 postcprocess/normalizeProcess.m
+++ 002 src/02 factor_conduct/04 postcprocess/normalizeProcess.m
@@ -1,4 +1,21 @@
-function [normFactor, meanValue, medianValue, skewnessValue, kurtosisValue] = normalizeProcess(factorMatrix)
+% function [normFactor, meanValue, medianValue, skewnessValue, kurtosisValue] = normalizeProcess(factorMatrix)
+% % NORMALIZEPROCESS returns the normalized version of feedStruct with
+% % z-score method 
+% % The normalization will be cross-sectional, which means row-wise
+% % x_i = (x_i - mean(x_i))/std(x_i)
+% % check validity of input: fieldName
+% 
+%     normFactor = factorMatrix;
+%     normFactor = zscore(normFactor, 0, 2);
+%     meanValue = mean(normFactor, 2);
+%     medianValue = median(normFactor, 2);
+%     skewnessValue = skewness(normFactor, 1, 2);
+%     kurtosisValue = kurtosis(normFactor, 1, 2);
+%     [m, ~] = size(normFactor);
+%     histogram(normFactor(m, :));
+% end
+
+function normFactor = normalizeProcess(factorMatrix)
 % NORMALIZEPROCESS returns the normalized version of feedStruct with
 % z-score method 
 % The normalization will be cross-sectional, which means row-wise
@@ -7,10 +24,10 @@ function [normFactor, meanValue, medianValue, skewnessValue, kurtosisValue] = no
 
     normFactor = factorMatrix;
     normFactor = zscore(normFactor, 0, 2);
-    meanValue = mean(normFactor, 2);
-    medianValue = median(normFactor, 2);
-    skewnessValue = skewness(normFactor, 1, 2);
-    kurtosisValue = kurtosis(normFactor, 1, 2);
-    [m, ~] = size(normFactor);
-    histogram(normFactor(m, :));
+%     meanValue = mean(normFactor, 2);
+%     medianValue = median(normFactor, 2);
+%     skewnessValue = skewness(normFactor, 1, 2);
+%     kurtosisValue = kurtosis(normFactor, 1, 2);
+%     [m, ~] = size(normFactor);
+%     histogram(normFactor(m, :));
 end
