8c3b0c087ae0cf8a5c4a3cd057f109b37ed5c13d
eiahb3838ya <eiahb3838ya@gmail.com>
2019-12-14 16:19:13 +0800

    update AlphaFactory
    saveAllAlphaHistory
    saveAllAlphaIncrement

--- 002 src/03 dailyFactorCreation/02 alphaFactoryModule/trytry.m
+++ 002 src/03 dailyFactorCreation/02 alphaFactoryModule/trytry.m
@@ -3,8 +3,8 @@ try
 catch
     disp('load data')
     load('projectData.mat');
-     Klass = AlphaFactory(projectData, 1, []);
+    Klass = AlphaFactory(projectData, 1, []);
 end
     
-Klass.saveAllAlphaHistory()
+
 
