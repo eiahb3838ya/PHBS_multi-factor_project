9dd4696bc9da083be9893b42fda213659c36286c
AlfredChenZH <alfredchen0228@163.com>
2019-12-30 17:18:03 +0800

    modify orth part

--- 002 src/04 factorNormalization/debugTest.m
+++ 002 src/04 factorNormalization/debugTest.m
@@ -1,5 +1,10 @@
 a = rand(8, 10);
 a = reshape(a, 2, 10, 4);
+c = rand(4, 10);
+c = reshape(c, 2, 10, 2);
+d = rand(4, 10);
+d = reshape(d, 2, 10, 2);
 
 b = FactorNormalization(a);
 b.calculateNorm();
+b.calculateOrth(c, d);
